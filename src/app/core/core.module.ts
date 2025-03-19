316354ad290bb5026d7b045d71a4680fb097fbdc
nura <nura@DESKTOP-8HSSN9E>
2025-03-19 12:26:24 +0330

    6-5

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './layers/footer/footer.component';
 import { NavbarComponent } from './layers/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule (
     {
@@ -10,7 +11,8 @@ import { NavbarComponent } from './layers/navbar/navbar.component';
             NavbarComponent
         ],
         imports:[
-            CommonModule
+            CommonModule,
+            RouterModule
         ],
         exports : [
             FooterComponent,

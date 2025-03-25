936dde3ad81f1332608115b98d938b5efe615c54
nura <nura@DESKTOP-8HSSN9E>
2025-03-25 17:54:36 +0330

    6-12

---
+++ src/app/shared/models/shopParams.ts
@@ -0,0 +1,10 @@
+export class shopParams
+{
+brandId? : number = 0 ;
+typeId? : number = 0 ;
+typeSort :  1 | 2 = 1 ; 
+search : string = '';
+sort  : number = 0 ;
+pageSize  : number = 10 ;
+pageIndex : number = 1 ;
+}
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/ipagination';
 import { IProduct } from '../shared/models/product';
 import {  Observable } from 'rxjs';
@@ -8,16 +8,41 @@ import { IType } from '../shared/models/type';
 import { IBrand } from '../shared/models/brand';
 import { Title } from '@angular/platform-browser';
 import {map} from 'rxjs/operators';
+import { shopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 private backendurl = environment.backendurl;
+private shopParams = new shopParams()
   constructor( private http : HttpClient) { }
+
+  getShopParams()
+  {
+    return this.shopParams;
+  }
+  updateShopParams(params : shopParams)
+  {
+this.shopParams = params;
+ 
+  }
   getProducts() : Observable<IPagination<IProduct>>
   {
- return this.http.get<IPagination<IProduct>>(environment.backendurl+'/products');
+    let params = this.generateShopParams();
+ return this.http.get<IPagination<IProduct>>(environment.backendurl+'/products' , );
+  }
+  private generateShopParams() {
+    let params = new HttpParams();
+    if (this.shopParams.search) params = params.append('search', this.shopParams.search);
+    if (this.shopParams.brandId && this.shopParams.brandId > 0) params = params.append('brandId', this.shopParams.brandId);
+    if (this.shopParams.typeId && this.shopParams.typeId > 0) params = params.append('typeId', this.shopParams.typeId);
+    params = params.append('pageIndex', this.shopParams.pageIndex);
+    params = params.append('pageSize', this.shopParams.pageSize);
+    params = params.append('sort', this.shopParams.sort);
+    params = params.append('typeSort', this.shopParams.typeSort);
+    console.log();
+    return params;
   }
   getBrands(includeAll=true)
   {
